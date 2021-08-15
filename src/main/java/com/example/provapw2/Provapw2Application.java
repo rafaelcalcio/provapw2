1ae3027237069b2b38f3f8f7a23ab469b948ca8d
pc <augustocalcio7@gmail.com>
2021-08-14 22:05:56 -0300

    alooo

---
+++ src/main/java/com/example/provapw2/Provapw2Application.java
@@ -0,0 +1,13 @@
+package com.example.provapw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Provapw2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Provapw2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/provapw2/Provapw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provapw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Provapw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
